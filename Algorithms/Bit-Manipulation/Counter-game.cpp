5dc72ca5310d00cfa7f7d3b152d257dbcb85a231
Anupam Srivastava <60839849+anuanu0-0@users.noreply.github.com>
2020-06-12 16:21:27 +0530

    Update Counter-game.cpp

--- Algorithms/Bit-Manipulation/Counter-game.cpp
+++ Algorithms/Bit-Manipulation/Counter-game.cpp
@@ -17,18 +17,20 @@
     N =        10           Richard will reduce counter by half
     N =        1            Louise can't make a move, hence, loses
                             Richard is the winner !
-    The above example shows that N=1101001100 can be better represented as "1101001100", where we need to count "1s to the left of 1" and "0s 
-    to the right of 1", to know the total number of operations.
+    The above example shows that N=1101001100 can be better represented as "1101001100", where we need to 
+    count "1s to the left of 1" and "0s to the right of 1", to know the total number of operations.
 
     Thus, total number of operations = 4 (1s in 1101001) + 2 (0s in 100) = 6. Since, this number (6) is even, hence, Richard wins.
 
 
-    This is all that is required to know the answer, but, as you can see, this forces us to do calculation in two parts : counting 1s and counting 0s.
-    Somehow, if we could modify the "100" part of "1101001100" to "011" thus changing the number to "1101001011", all we would need is to count 1s in 
-    this number i.e. setBits in the number.
+    This is all that is required to know the answer, but, as you can see, this forces us to do calculation in two parts : 
+        -> counting 1s and -> counting 0s.
+    Somehow, if we could modify the "100" part of "1101001100" to "011" thus changing the number to "1101001011", all we would 
+    need is to count 1s in this number i.e. setBits in the number.
 
-    This final requirement to flip all trailing 0s to 1s and last 1 to 0 (without modifying the other bits), can be easily achieved by subtracting 
-    1 from the number N. For N=1101001100, we have N-1 = 1101001011. Thus popcount(N-1) gives the number of operations in the game, which gives winner 
+    This final requirement to flip all trailing 0s to 1s and last 1 to 0 (without modifying the other bits), can be easily 
+    achieved by subtracting 1 from the number N. For N=1101001100, we have N-1 = 1101001011. Thus popcount(N-1) gives the number
+    of operations in the game, which gives winner 
     depending upon if its even or odd.
 
 */
